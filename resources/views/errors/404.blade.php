cc4ead11728540328891a5eb87d680926b505f6c
RobertoZeppilli <robzep171@gmail.com>
2021-07-19 14:17:10 +0200

    init: db, migrate, seed, homeview, errors

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <section>
+        <h5>Error</h5>
+    </section>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <section>
+        <h2>Main</h2>
+    </section>
+@endsection
